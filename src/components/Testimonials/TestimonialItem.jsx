132aee412b080f6922db051df660faec59a154f2
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2022-03-15 13:01:07 +0100

    testimonials completed

---
+++ src/components/Testimonials/TestimonialItem.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import classes from "./Testimonials.module.css";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const TestimonialItem = ({ testimonials }) => {
+  return (
+    <Swiper
+      className={`container ${classes.testimonials__container}`}
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+    >
+      {testimonials.map(({ id, image, name, review }) => {
+        return (
+          <SwiperSlide className={classes.testimonial}>
+            <div className={classes.client__avatar}>
+              <img src={image} alt={name} />
+            </div>
+            <h5 className={classes.client__name}>{name}</h5>
+            <small className={classes.client__review}>{review}</small>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
+
+export default TestimonialItem;
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,7 +1,52 @@
 import React from "react";
-// import classes from "./Testimonials.module.css";
+
+import TestimonialItem from "./TestimonialItem";
+import AVTR1 from "../../assets/avatar_1.jpg";
+import AVTR2 from "../../assets/avatar_2.jpg";
+import AVTR3 from "../../assets/avatar_3.jpg";
+import AVTR4 from "../../assets/avatar_4.webp";
+
+const testimonials = [
+  {
+    id: 1,
+    image: AVTR1,
+    name: "Ernest Achiever",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, repellat. Asperiores deleniti aut consequuntur accusantium itaque consequatur tempore nemo laborum.",
+  },
+
+  {
+    id: 2,
+    image: AVTR2,
+    name: "John Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, repellat. Asperiores deleniti aut consequuntur accusantium itaque consequatur tempore nemo laborum.",
+  },
+  {
+    id: 3,
+    image: AVTR3,
+    name: "Felix Divine",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, repellat. Asperiores deleniti aut consequuntur accusantium itaque consequatur tempore nemo laborum.",
+  },
+
+  {
+    id: 4,
+    image: AVTR4,
+    name: "Pamela Achiever",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, repellat. Asperiores deleniti aut consequuntur accusantium itaque consequatur tempore nemo laborum.",
+  },
+];
+
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <TestimonialItem testimonials={testimonials} />
+    </section>
+  );
 };
 
 export default Testimonials;
